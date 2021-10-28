4f8dfe2ea65b9bcd87b144c8eef4a1516c7c8b5c
kollarp <kollar.pal@molaris.hu>
2021-10-28 21:20:19 +0200

    Added async file import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,83 @@
-import { Component } from '@angular/core'
+import { Component } from "@angular/core";
+import { File, knownFolders, path } from "@nativescript/core";
 
+import {
+  FilePickerOptions,
+  openFilePicker,
+} from "@nativescript-community/ui-document-picker";
+import { merge, Observable } from "rxjs";
+
+const FILE_PICKER_OPTIONS: FilePickerOptions = {
+  extensions: ["epub", "mobi", "pdf"],
+  multipleSelection: false,
+};
+
+interface PickerData {
+  files: string[];
+  ios?: any;
+  android?: any;
+}
 @Component({
-  selector: 'ns-app',
-  templateUrl: './app.component.html',
+  selector: "ns-app",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly currentAppFolder = knownFolders.currentApp();
+
+  onTap(): void {
+    openFilePicker(FILE_PICKER_OPTIONS).then((d) => this.handleFileImport(d));
+  }
+
+  private handleFileImport({ files, ios, android }: PickerData): void {
+    console.log({ files, android });
+    if (0 < files.length) {
+      const imports$ = files?.map((filePath) => this.importFileAsync(filePath));
+
+      merge(...imports$).subscribe({
+        next: (n) => console.log(n),
+        error: (e) => console.error(e),
+        complete: () => console.log("COMPLETE"),
+      });
+    }
+  }
+
+  /**
+   * This method throws the error below:
+   * Error: ReadTask returns no result.
+   */
+  private importFileAsync(filePath: string): Observable<any> {
+    const sourceFile = this.createSourceFile(filePath);
+    const destinationFile = this.createDestinationFile(filePath);
+    return new Observable((observer) => {
+      const catchError = (e: any) => {
+        observer.error(e);
+        observer.complete();
+      };
+      sourceFile
+        .read()
+        .then((binary) => {
+          destinationFile
+            .write(binary)
+            .then(() => {
+              observer.next("SUCCESS");
+              observer.complete();
+            })
+            .catch((e) => catchError(e));
+        })
+        .catch((e) => catchError(e));
+    });
+  }
+
+  private createSourceFile(filePath: string): File {
+    return File.fromPath(filePath);
+  }
+
+  private createDestinationFile(filePath: string): File {
+    const fileName = this.getFileNameFromFilePath(filePath);
+    return File.fromPath(path.join(this.currentAppFolder.path, fileName));
+  }
+
+  private getFileNameFromFilePath(filePath: string): string {
+    return filePath.split("\\").pop().split("/").pop();
+  }
+}
